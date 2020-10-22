40be257ccd1cddc6cc59a35310c2577a97741ba0
Raoul Rubien <rubienr@sbox.tugraz.at>
2020-10-22 21:14:50 +0200

    config update

--- private/config/btt-skr-pro-v1.1/Configuration-2.6.0.h
+++ private/config/btt-skr-pro-v1.1/Configuration-2.6.0.h
@@ -133,9 +133,11 @@
   #define Y_MAX_PIN       PE12 // use Y- pin
   #define FIL_RUNOUT_PIN  PE15 // E0 aka X+ pin
   #define FIL_RUNOUT2_PIN PE10 // E1 aka Y+ pin
-  #define FIL_RUNOUT3_PIN PEB5 // E2 aka Z+ pin
-  #define PS_ON_PIN       PD0  // EXPANSION 2
-  #define CASE_LIGHT_PIN  PE4  // Expansion 2
+  #define FIL_RUNOUT3_PIN PB5  // E2 aka Z+ pin
+  #define PS_ON_PIN       PD0  // Extension 2
+  #define CASE_LIGHT_PIN  PE4  // Extension 2
+  #define SERVO1_PIN      PC9  // Extension 1
+  #define SERVO2_PIN      PF8  // Extension 1
   #define SUICIDE_PIN_INVERTING false
   #define FLASH_EEPROM_EMULATION
 #endif
@@ -151,10 +153,11 @@
 
 // This defines the number of extruders
 // :[0, 1, 2, 3, 4, 5, 6, 7, 8]
-#define EXTRUDERS 1
+#define EXTRUDERS 2
 
 // Generally expected filament diameter (1.75, 2.85, 3.0, ...). Used for Volumetric, Filament Width Sensor, etc.
 #define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
+
 
 // For Cyclops or any "multi-extruder" that shares a single nozzle.
 //#define SINGLENOZZLE
@@ -206,10 +209,10 @@
 #endif
 
 // A dual-nozzle that uses a servomotor to raise/lower one (or both) of the nozzles
-//#define SWITCHING_NOZZLE
+#define SWITCHING_NOZZLE
 #if ENABLED(SWITCHING_NOZZLE)
-  #define SWITCHING_NOZZLE_SERVO_NR 0
-  //#define SWITCHING_NOZZLE_E1_SERVO_NR 1          // If two servos are used, the index of the second
+  #define SWITCHING_NOZZLE_SERVO_NR 1
+  #define SWITCHING_NOZZLE_E1_SERVO_NR 2          // If two servos are used, the index of the second
   #define SWITCHING_NOZZLE_SERVO_ANGLES { 0, 90 }   // Angles for E0, E1 (single servo) or lowered/raised (dual servo)
 #endif
 
@@ -246,7 +249,7 @@
 
     #define MPE_FAST_SPEED      9000      // (mm/m) Speed for travel before last distance point
     #define MPE_SLOW_SPEED      4500      // (mm/m) Speed for last distance travel to park and couple
-    #define MPE_TRAVEL_DISTANCE   10      // (mm) Last distance point
+    #define MPE_DISTANCE   10      // (mm) Last distance point
     #define MPE_COMPENSATION       0      // Offset Compensation -1 , 0 , 1 (multiplier) only for coupling
 
   #endif
@@ -323,7 +326,7 @@
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
 // The offset has to be X=0, Y=0 for the extruder 0 hotend (default extruder).
 // For the other hotends it is their distance from the extruder 0 hotend.
-#define HOTEND_OFFSET_X { 0.0, 39.00 } // (mm) relative X-offset for each nozzle
+#define HOTEND_OFFSET_X { 0.0, -37.95 } // (mm) relative X-offset for each nozzle
 #define HOTEND_OFFSET_Y { 0.0, 0.00 }  // (mm) relative Y-offset for each nozzle
 #define HOTEND_OFFSET_Z { 0.0, 0.00 }  // (mm) relative Z-offset for each nozzle
 
@@ -347,8 +350,8 @@
   #define AUTO_POWER_CONTROL       // Enable automatic control of the PS_ON pin
   #if ENABLED(AUTO_POWER_CONTROL)
     #define AUTO_POWER_FANS      // Turn on PSU if fans need power
-    //#define AUTO_POWER_E_FANS
-    //#define AUTO_POWER_CONTROLLERFAN
+    #define AUTO_POWER_E_FANS
+    #define AUTO_POWER_CONTROLLERFAN
     //#define AUTO_POWER_CHAMBER_FAN
     #define AUTO_POWER_E_TEMP        50 // (°C) Turn on PSU over this temperature
     //#define AUTO_POWER_CHAMBER_TEMP  30 // (°C) Turn on PSU over this temperature
@@ -426,7 +429,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
 #define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_1 0
+#define TEMP_SENSOR_1 1
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
@@ -499,9 +502,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Creality Ender-5 Plus, auto tune result of: M303 E0 S225 C10
-  #define  DEFAULT_Kp 19.41
-  #define  DEFAULT_Ki 1.38
-  #define  DEFAULT_Kd 68.38
+  #define DEFAULT_Kp 29.49
+  #define DEFAULT_Ki 2.88
+  #define DEFAULT_Kd 75.50
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -537,7 +540,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -553,15 +556,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-
-  // TODO rubienr auto tune: Still has an issue with "PID Autotune failed! Bad extruder number" -> auto tune postponed.
   // Creality Ender-5 Plus, auto tune result of: M303 E-1 S60 C10
+  #define DEFAULT_bedKp 161.74
+  #define DEFAULT_bedKi 21.06
+  #define DEFAULT_bedKd 828.06
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -597,7 +601,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 1000
+#define EXTRUDE_MAXLENGTH 200
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
@@ -767,25 +771,27 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 
-// X/Y steps example: steps per revolution s=200, microstepping m=16, effective gear diameter d=40.00: sm/(πd) =  80.0
-//   Z steps example: steps per revolution s=200, microstepping m=16, effective gear distance d= 4.00: sm/d    = 800.0
-//   E steps example: steps per revolution s=200, microstepping m=16, effective gear diameter d= 7.22: sm/(πd) = 144.1
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 144.1 }
+// circle diameter  d
+// circumference    c = πd
+//
+// X/Y steps example: steps per revolution s=200, microstepping m=16, effective gear circumference c=40.00
+//   sm/c =  80.0
+// Z steps example: steps per revolution s=200, microstepping m=16, effective spindle thread distance d= 4.00
+//   sm/d    = 80
+// E steps example: steps per revolution s=200, microstepping m=16, effective gear diameter d= 7.22
+//   sm/(πd) = 144.1
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 120.93manholecoverate }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-// with classic jerk
-//#define DEFAULT_MAX_FEEDRATE          { 8000, 8000, 15, 500 }
-// with junciton deviation
-//#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 15, 200 }
-#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 15, 1500 }
+#define DEFAULT_MAX_FEEDRATE          { 1500, 1500, 30, 1500 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 1500, 1500, 30, 400 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 1500, 1500, 30, 1500 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -794,9 +800,6 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-// TODO rubienr - with classic jerk:
-//#define DEFAULT_MAX_ACCELERATION      { 15000, 15000, 100, 15000 }
-// TODO rubienr - with junction deviation:
 #define DEFAULT_MAX_ACCELERATION      { 5000, 5000, 100, 15000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
@@ -812,14 +815,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-// TODO rubienr - with classic jerk:
-//#define DEFAULT_ACCELERATION          8000    // X, Y, Z and E acceleration for printing moves
-//#define DEFAULT_RETRACT_ACCELERATION  8000    // E acceleration for retracts
-//#define DEFAULT_TRAVEL_ACCELERATION  12000    // X, Y, Z acceleration for travel (non printing) moves
-// TODO rubienr - with junction deviation:
 #define DEFAULT_ACCELERATION           1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  15000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION    1500    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_RETRACT_ACCELERATION   15000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION    2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
@@ -1024,14 +1022,14 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -5, -4.3 }
+#define NOZZLE_TO_PROBE_OFFSET { -26.5, +47.5, -2.00 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 5
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 11000
+#define XY_PROBE_SPEED 500*60
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
@@ -1133,7 +1131,7 @@
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
 #define INVERT_E0_DIR true
-#define INVERT_E1_DIR true
+#define INVERT_E1_DIR false
 #define INVERT_E2_DIR true
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
@@ -1164,16 +1162,16 @@
 
 // The size of the print bed
 // TODO rubienr: corresponds to max x/y axis travel, not to bed size
-#define X_BED_SIZE 366
-#define Y_BED_SIZE 377
+#define X_BED_SIZE 369
+#define Y_BED_SIZE 370 // +28 => axis total length
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 // TODO rubienr (x,y)=(0,0) corresponds to bed corner
-#define X_MIN_POS  8
+#define X_MIN_POS  25
 #define Y_MIN_POS -8
 #define Z_MIN_POS  0
 #define X_MAX_POS  X_BED_SIZE + X_MIN_POS
-#define Y_MAX_POS  Y_BED_SIZE + Y_MIN_POS
+#define Y_MAX_POS  Y_BED_SIZE + 28 + Y_MIN_POS
 #define Z_MAX_POS  425        + Z_MIN_POS
 
 /**
@@ -1214,7 +1212,7 @@
  */
 #define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
-  #define NUM_RUNOUT_SENSORS   1     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
+  #define NUM_RUNOUT_SENSORS   2     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
   #define FIL_RUNOUT_STATE     LOW   // Pin state indicating that filament is NOT present.
   #define FIL_RUNOUT_PULLUP          // Use internal pullup for filament runout pins.
   //#define FIL_RUNOUT_PULLDOWN      // Use internal pulldown for filament runout pins.
@@ -1308,13 +1306,13 @@
   /**
    * Enable the G26 Mesh Validation Pattern tool.
    */
-  //#define G26_MESH_VALIDATION
+  #define G26_MESH_VALIDATION
   #if ENABLED(G26_MESH_VALIDATION)
     #define MESH_TEST_NOZZLE_SIZE    0.4  // (mm) Diameter of primary nozzle.
-    #define MESH_TEST_LAYER_HEIGHT   0.2  // (mm) Default layer height for the G26 Mesh Validation Tool.
-    #define MESH_TEST_HOTEND_TEMP  205    // (°C) Default nozzle temperature for the G26 Mesh Validation Tool.
+    #define MESH_TEST_LAYER_HEIGHT   0.3  // (mm) Default layer height for the G26 Mesh Validation Tool.
+    #define MESH_TEST_HOTEND_TEMP  210    // (°C) Default nozzle temperature for the G26 Mesh Validation Tool.
     #define MESH_TEST_BED_TEMP      60    // (°C) Default bed temperature for the G26 Mesh Validation Tool.
-    #define G26_XY_FEEDRATE         20    // (mm/s) Feedrate for XY Moves for the G26 Mesh Validation Tool.
+    #define G26_XY_FEEDRATE      20000    // (mm/s) Feedrate for XY Moves for the G26 Mesh Validation Tool.
     #define G26_RETRACT_MULTIPLIER   1.0  // G26 Q (retraction) used by default between mesh test elements.
   #endif
 
@@ -1545,13 +1543,13 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 185
+#define PREHEAT_1_TEMP_HOTEND 180
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_TEMP_HOTEND 220
+#define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
@@ -1615,36 +1613,52 @@
  * Attention: EXPERIMENTAL. G-code arguments may change.
  *
  */
-//#define NOZZLE_CLEAN_FEATURE
+#define NOZZLE_CLEAN_FEATURE
 
 #if ENABLED(NOZZLE_CLEAN_FEATURE)
   // Default number of pattern repetitions
-  #define NOZZLE_CLEAN_STROKES  12
+  #define NOZZLE_CLEAN_STROKES  1
 
   // Default number of triangles
-  #define NOZZLE_CLEAN_TRIANGLES  3
+  #define NOZZLE_CLEAN_TRIANGLES  12
 
   // Specify positions for each tool as { { X, Y, Z }, { X, Y, Z } }
   // Dual hotend system may use { {  -20, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) },  {  420, (Y_BED_SIZE / 2), (Z_MIN_POS + 1) }}
-  #define NOZZLE_CLEAN_START_POINT { {  30, 30, (Z_MIN_POS + 1) } }
-  #define NOZZLE_CLEAN_END_POINT   { { 100, 60, (Z_MIN_POS + 1) } }
+  #define NOZZLE_CLEAN_START_POINT_X_ANY_TOOL  80
+  #define NOZZLE_CLEAN_START_POINT_Y_ANY_TOOL 375
+  #define NOZZLE_CLEAN_DX_MM 28
+  #define NOZZLE_CLEAN_DY_MM 15
+
+  #define NOZZLE_CLEAN_START_POINT_ANY_TOOL {  NOZZLE_CLEAN_START_POINT_X_ANY_TOOL, NOZZLE_CLEAN_START_POINT_Y_ANY_TOOL, (Z_MIN_POS + 1) }
+  #define NOZZLE_CLEAN_END_POINT_ANY_TOOL { ( NOZZLE_CLEAN_START_POINT_X_ANY_TOOL + NOZZLE_CLEAN_DX_MM ) , ( NOZZLE_CLEAN_START_POINT_Y_ANY_TOOL + NOZZLE_CLEAN_DY_MM) , (Z_MIN_POS + 1) }
+  #define NOZZLE_CLEAN_START_POINT { NOZZLE_CLEAN_START_POINT_ANY_TOOL, NOZZLE_CLEAN_START_POINT_ANY_TOOL }
+  #define NOZZLE_CLEAN_END_POINT   { NOZZLE_CLEAN_END_POINT_ANY_TOOL, NOZZLE_CLEAN_END_POINT_ANY_TOOL }
 
   // Circular pattern radius
-  #define NOZZLE_CLEAN_CIRCLE_RADIUS 6.5
+  #define NOZZLE_CLEAN_CIRCLE_RADIUS 5
   // Circular pattern circle fragments number
   #define NOZZLE_CLEAN_CIRCLE_FN 10
   // Middle point of circle
   #define NOZZLE_CLEAN_CIRCLE_MIDDLE NOZZLE_CLEAN_START_POINT
 
   // Move the nozzle to the initial position after cleaning
-  #define NOZZLE_CLEAN_GOBACK
+  //#define NOZZLE_CLEAN_GOBACK
 
   // Enable for a purge/clean station that's always at the gantry height (thus no Z move)
-  //#define NOZZLE_CLEAN_NO_Z
+  #define NOZZLE_CLEAN_NO_Z
 
   // Explicit wipe G-code script applies to a G12 with no arguments.
   //#define WIPE_SEQUENCE_COMMANDS "G1 X-17 Y25 Z10 F4000\nG1 Z1\nM114\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 X-17 Y25\nG1 X-17 Y95\nG1 Z15\nM400\nG0 X-10.0 Y-9.0"
-
+  #define WIPE_SEQUENCE_COMMANDS "\
+    G0 F30000\n\
+    T1\n\
+    G0 X75 Y375 \n\
+    G12 P1\n\
+    G0 Y375\n\
+    T0\n\
+    G0 X75\n\
+    G12 P1\n\
+    G0 Y375"
 #endif
 
 /**
@@ -2324,7 +2338,7 @@
   //#define NEOPIXEL2_TYPE NEO_GRB
   //#define NEOPIXEL2_PIN        5
   #define NEOPIXEL_PIXELS       27  // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
-  //#define NEOPIXEL_IS_SEQUENTIAL  // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
+  #define NEOPIXEL_IS_SEQUENTIAL  // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
   #define NEOPIXEL_BRIGHTNESS  180  // Initial brightness (0-255)
   //#define NEOPIXEL_STARTUP_TEST   // Cycle through colors at startup
 
@@ -2360,15 +2374,15 @@
  * Set this manually if there are extra servos needing manual control.
  * Leave undefined or set to 0 to entirely disable the servo subsystem.
  */
-//#define NUM_SERVOS 3 // Servo index starts with 0 for M280 command
+#define NUM_SERVOS 3 // Servo index starts with 0 for M280 command
 
 // (ms) Delay  before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
 // If the servo can't reach the requested position, increase it.
-#define SERVO_DELAY { 300 }
+#define SERVO_DELAY { 300, 300, 300 }
 
 // Only power servos during movement, otherwise leave off to prevent jitter
-//#define DEACTIVATE_SERVOS_AFTER_MOVE
+#define DEACTIVATE_SERVOS_AFTER_MOVE
 
 // Allow servo angle to be edited and saved to EEPROM
 //#define EDITABLE_SERVO_ANGLES
